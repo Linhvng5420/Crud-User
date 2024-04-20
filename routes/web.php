92561c5de533901996e4734310f2af0769e3471d
Linh Van Nguyen <linhvng5420@gmail.com>
2024-04-20 16:21:08 +0700

    Bo nut 'chinh sua' khoi view

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,6 @@ Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user
 
 Route::get('viewUser', [CrudUserController::class, 'viewUser'])->name('user.viewUser');
 
-
-
 Route::get('/', function () {
     return view('crud.login');
 });
